928a91045f18ae2af698cb8014ab708ab652aeed
ashraf604 <ashrafhanafy606@gmail.com>
2025-08-12 11:34:56 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,18 @@
 const { MongoClient } = require("mongodb");
 
+// --- DEBUGGING CODE START ---
+// هذا الكود سيطبع قيمة المتغير في الـ Logs لنرى ما هي المشكلة
+console.log("--- DEBUGGING MONGO_URI ---");
+const uri_for_debugging = process.env.MONGO_URI;
+console.log("Is MONGO_URI variable present?", !!uri_for_debugging);
+console.log("Type of MONGO_URI:", typeof uri_for_debugging);
+if (uri_for_debugging) {
+    console.log("Value of MONGO_URI:", uri_for_debugging.substring(0, 15) + "..."); // نطبع أول 15 حرفًا فقط للأمان
+}
+console.log("--- END DEBUGGING ---");
+// --- DEBUGGING CODE END ---
+
+
 const uri = process.env.MONGO_URI;
 
 if (!uri) {
